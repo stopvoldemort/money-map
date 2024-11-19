759b18af8d1907029ad33190af1eefa1ee3cfa5f
stopvoldemort <david.squires1@gmail.com>
2024-11-19 15:02:42 -0500

    use default port for frontend

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$NODE_ENV" = "development" ]; then
     echo "Running Development Server"
-    exec npm start -- --port $PORT
+    exec npm start
 else
     echo "Running Production Server"
-    exec serve -s dist -l $PORT
+    exec serve -s dist
 fi
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  console.log("FRONTEND ENV:", env);
 
   return defineConfig({
     plugins: [react()],
     server: {
-      port: Number(env.PORT) || 8080,
       proxy: {
         "/api": {
-          target: "http://backend:5000", // Your Flask backend
+          target: env.VITE_BACKEND_URL,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api /, "/api"),
