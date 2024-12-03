cde2b57edac7681231f802eafd35e6527652d684
David Squires <david.squires@takeoff.com>
2024-12-02 20:43:13 -0500

    store form data in browser

--- frontend/src/pages/ChartAndFormPage.tsx
+++ frontend/src/pages/ChartAndFormPage.tsx
@@ -1,14 +1,25 @@
 import { useMutation } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { Container, Row } from "react-bootstrap";
 import ChartComponent, { NetWorthChartData } from "../components/results/ChartComponent";
 import FormComponent from "../components/form/FormComponent";
 import { FormValuesType } from "../components/form/types";
 import { initialValues } from "../components/form/initialValues";
+declare const FORM_VERSION: string;
 
 const ChartAndFormPage: React.FC = () => {
   const [chartData, setChartData] = useState<NetWorthChartData[]>([]);
+
+  const values = useMemo(() => {
+    let values = initialValues;
+    const savedFormJSON = localStorage.getItem(FORM_VERSION)
+    if (savedFormJSON) {
+      console.log("Using saved values")
+      values = JSON.parse(savedFormJSON)
+    }
+    return values;
+  }, []);
 
   const mutation = useMutation({
     mutationFn: async (formData: FormValuesType) => {
@@ -34,11 +45,12 @@ const ChartAndFormPage: React.FC = () => {
 
   const handleUpdate = (formData: FormValuesType) => {
     console.log("Form Data:", formData);
+    localStorage.setItem(FORM_VERSION, JSON.stringify(formData));
     mutation.mutate(formData);
   };
 
   useEffect(() => {
-    handleUpdate(initialValues);
+    handleUpdate(values);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -51,6 +63,7 @@ const ChartAndFormPage: React.FC = () => {
       </Row>
       <Row className="my-5">
         <FormComponent
+          initialValues={values}
           onUpdate={handleUpdate}
         />
       </Row>
