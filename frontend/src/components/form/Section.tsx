21ef36f665f943e5dc5c2fd5f45876acea90490a
David Squires <david.squires@takeoff.com>
2024-12-09 14:47:48 -0500

    number of items displayed in form

--- frontend/src/components/form/Section.tsx
+++ frontend/src/components/form/Section.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { Accordion } from 'react-bootstrap';
 import InfoPopover from './InfoPopover';
 
-const Section = ({ title, infoText, children }: { title: string, infoText?: string, children: React.ReactNode }) => {
+const CustomHeader = ({ title, summary, infoText }: { title: string, summary?: string, infoText?: string }) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+      <div>
+        {title}
+        {infoText && (<InfoPopover text={infoText} />)}
+      </div>
+      {summary && <span className="text-muted text-end mx-5">{summary}</span>}
+    </div>
+  )
+}
+
+const Section = ({ title, summary, infoText, children }: { title: string, summary?: string, infoText?: string, children: React.ReactNode }) => {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <Accordion.Item eventKey={title}>
       <Accordion.Header onClick={() => setIsOpen(!isOpen)}>
-        {title}
-        {infoText && (<InfoPopover text={infoText} />)}
+        <CustomHeader title={title} summary={summary} infoText={infoText} />
       </Accordion.Header>
       <Accordion.Body>
         {isOpen && children}
